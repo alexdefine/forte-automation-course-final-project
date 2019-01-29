350e3c06d937dd875d46666191f3cd339df8597d
Alex Underfiled <alexwooppy@gmail.com>
2019-01-29 12:08:09 +0200

    added VerifyErrorMessages test added LoginPage object

---
+++ Forte.AutomationCourses.FinalProject.Tests/PageObjects/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Forte.AutomationCourses.FinalProject.Tests.PageObjects
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://phptravels.org/clientarea.php");
+        }
+
+        public void LogIn(string email, string password)
+        {
+            driver.FindElement(By.Id("inputEmail")).SendKeys(email);
+            driver.FindElement(By.Id("inputPassword")).SendKeys(password);
+            driver.FindElement(By.Id("login")).Submit();
+        }
+
+        public IWebDriver GetDriver()
+        {
+            return driver;
+        }
+    }
+}
---
+++ Forte.AutomationCourses.FinalProject.Tests/TestSuite001.cs
@@ -0,0 +1,35 @@
+﻿using Forte.AutomationCourses.FinalProject.Tests.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forte.AutomationCourses.FinalProject.Tests
+{
+    [TestFixture]
+    public class TestSuite001
+    {
+        [Test]
+        public void VerifyErrorMessages()
+        {
+            var loginPage = new LoginPage();
+            loginPage.LogIn("wrong@email.com", "password");
+
+            try
+            {
+                if (loginPage.GetDriver().FindElement(By.ClassName("alert-danger")).Text != "Login Details Incorrect. Please try again.")
+                {
+                    throw new Exception("Error");
+                }
+            }
+            catch (StaleElementReferenceException)
+            {
+                Console.WriteLine("stale element reference exception was caught");
+            }
+        }
+    }
+}
